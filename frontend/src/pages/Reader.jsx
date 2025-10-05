83001a56424a5e41938e95873acae428d25cad18
ryuucxo01 <ryuucxo01@gmail.com>
2025-10-06 02:27:43 +0700

    Create Reader component

---
+++ frontend/src/pages/Reader.jsx
@@ -0,0 +1,13 @@
+// Page for reading Quran with audio player
+import React from 'react';
+
+const Reader = () => {
+    return (
+        <div>
+            <h1>Quran Reader</h1>
+            {/* Audio player goes here */}
+        </div>
+    );
+};
+
+export default Reader;
